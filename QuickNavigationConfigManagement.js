e022af02f4c33f72b0a3f1d963667b7c1198d982
Tom-Laycock <Tom.Laycock@tombola.com>
2022-02-07 11:59:46 +0000

    Quick Nav Update
    - Saving to local storage
    - Loading from local storage
    - Displaying config with buttons animations and text
    - Config overwriting

--- QuickNavigationConfigManagement.js
+++ QuickNavigationConfigManagement.js
@@ -1,4 +1,5 @@
 var Configs = {}
+var ConfigNames = []
 
 function readFile(file, onLoadCallback) {
     var reader = new FileReader();
@@ -6,19 +7,21 @@ function readFile(file, onLoadCallback) {
     reader.readAsText(file);
 }
 
-function loadConfig() {
+function loadConfig(setAsActive = true, updateConfigsInStorage = true) {
     let QuickNavigationConfigFile = document.getElementById("quick_nav_config_upload").files[0];
-
-    if (!QuickNavigationConfigFile == null) {
+    if (QuickNavigationConfigFile != null) {
         readFile(QuickNavigationConfigFile, function(e) {
             var QuickNavigationConfigContent = e.target.result;
             var QuickNavigationConfigJSON = JSON.parse(QuickNavigationConfigContent);
             var config = convertJSONtoConfig(QuickNavigationConfigJSON);
 
-            if (Configs[QuickNavigationConfigJSON.Name] != null) { console.warn("A config with the name: " + QuickNavigationConfigJSON.Name + " already exists!"); return; }
+            // if (Configs[QuickNavigationConfigJSON.Name] != null) { console.warn("A config with the name: " + QuickNavigationConfigJSON.Name + " already exists!"); return; }
             Configs[QuickNavigationConfigJSON.Name] = config;
+            ConfigNames.push(QuickNavigationConfigJSON.Name);
 
-            addConfigToSelector(QuickNavigationConfigJSON.Name, true);
+            window.localStorage.setItem(QuickNavigationConfigJSON.Name, JSON.stringify(config));
+
+            addConfigToSelector(QuickNavigationConfigJSON.Name, setAsActive, updateConfigsInStorage);
         });
     }
 
@@ -30,20 +33,63 @@ function convertJSONtoConfig(configJSON) {
 
     config.Name = configJSON.Name;
 
+    configJSON.Sections.forEach(element => {
+        var newSection = new Section();
+        newSection.Name = element.Name;
+        element.QuickNavLinks.forEach(quickNavLink => {
+            var newQuickNavLink = new QuickNavLink();
+            newQuickNavLink.Name = quickNavLink.Name;
+            newQuickNavLink.Link = quickNavLink.Link;
+            newQuickNavLink.Target = quickNavLink.Target;
+            newSection.QuickNavLinks.push(newQuickNavLink);
+        });
+        config.Sections.push(newSection);
+    });
+
     return config;
 }
 
-function activateConfig(configName) {
-    console.log(Configs[configName]);
-
+function activateConfig(configName, setAsActiveConfig = true) {
     document.getElementById("quick_nav_title").innerHTML = Configs[configName].Name;
+
+    var container = document.getElementById("quick_nav_section_container");
+    while (container.firstChild) {
+        container.removeChild(container.lastChild);
+    }
+
+    Configs[configName].Sections.forEach(section => {
+        var newSection = CreateSection(section.Name);
+        section.QuickNavLinks.forEach(quickNavLink => {
+            AddNavLinkToSection(newSection, quickNavLink);
+        });
+        container.appendChild(newSection);
+        AddFinalDetailsToSection(newSection);
+    });
+
+    if (setAsActiveConfig) {
+        window.localStorage.setItem("CurrentConfig", configName);
+    }
 }
 
-function addConfigToSelector(configToAdd, setAsActive = false) {
+function addConfigToSelector(configToAdd, setAsActive = false, updateConfigsInStorage = false, setAsActiveConfig = true) {
     var configSelector = document.getElementById("config_select");
 
     Array.from(configSelector.options).forEach(function(option_element) {
-        if (option_element.text == configToAdd) { console.warn("A config with the name: " + QuickNavigationConfigJSON.Name + " already exists!"); return; }
+        if (option_element.text == configToAdd) {
+            console.warn("A config with the name: " + configToAdd + " already exists!");
+
+            configSelector.value = configToAdd;
+
+            if (setAsActive) {
+                configSelector.value = configToAdd;
+                activateConfig(configToAdd, setAsActiveConfig);
+            }
+
+            if (updateConfigsInStorage) {
+                UpdateLocalConfigStorage();
+            }
+            return;
+        }
     });
 
     var option = document.createElement("option");
@@ -57,7 +103,11 @@ function addConfigToSelector(configToAdd, setAsActive = false) {
 
     if (setAsActive) {
         configSelector.value = configToAdd;
-        activateConfig(configToAdd);
+        activateConfig(configToAdd, setAsActiveConfig);
+    }
+
+    if (updateConfigsInStorage) {
+        UpdateLocalConfigStorage();
     }
 }
 
@@ -66,10 +116,24 @@ function createDefaultConfig() {
 
     config.Name = "Default";
 
-    Configs[config.Name] = config;
-    addConfigToSelector(config.Name, true);
+    var WebsitesSection = {
+        "Name": "Websites",
+        "QuickNavLinks": [{
+            "Name": "Google",
+            "Link": "https://www.google.co.uk",
+            "Target": "_blank"
+        }]
+    };
 
-    activateConfig(config.Name);
+    config.Sections.push(WebsitesSection);
+
+    Configs[config.Name] = config;
+    ConfigNames.push(config.Name);
+    addConfigToSelector(config.Name, true, false, false);
+}
+
+function UpdateLocalConfigStorage() {
+    window.localStorage.setItem("ConfigNames", JSON.stringify(ConfigNames));
 }
 
 var LoadConfigMenuOpen = false
@@ -86,7 +150,31 @@ function toggleLoadConfigMenu() {
     }
 }
 
+function InitialiseQuickNavigation() {
+    var CurrentConfigs = JSON.parse(window.localStorage.getItem("ConfigNames"));
+
+    var CurrentActiveConfig = window.localStorage.getItem("CurrentConfig");
+    console.log(CurrentActiveConfig);
+    if (CurrentConfigs == null) { return; }
+
+    CurrentConfigs.forEach(configName => {
+        if (configName != "Default") {
+            var LoadedConfig = JSON.parse(window.localStorage.getItem(configName));
+            Configs[configName] = LoadedConfig;
+            console.log(LoadedConfig);
+            addConfigToSelector(configName, false, false);
+            if (configName == CurrentActiveConfig) {
+                console.log("ACTIVATING CONFIG  " + configName);
+                activateConfig(configName);
+            }
+        }
+    });
+}
+
 window.onload = function() {
+    createDefaultConfig();
+    InitialiseQuickNavigation();
+
     let ConfigSubmitButton = document.getElementById("quick_nav_config_submit_button");
     ConfigSubmitButton.addEventListener("click", loadConfig);
 
@@ -97,8 +185,6 @@ window.onload = function() {
     Array.from(CloseButtons).forEach(function(button) {
         button.addEventListener("click", toggleLoadConfigMenu);
     });
-
-    createDefaultConfig();
 
     const configSelector = document.getElementById("config_select");
 
